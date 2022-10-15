3bbc8cffe07ee0a27244aa1b2f397b067344f53d
Alazarad <Alazarad>
2022-10-14 18:30:44 -0700

    tell if random number is positive or negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#!/bin/bash
+#include <stdlib.h>
+#include <time.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (x > 0)
+	{
+		printf("The number is positive /n");
+	}
+	else if (x < 0) 
+	{
+		printf("The number is negative /n");
+	}
+	else
+	{
+		printf("The number is zero /n");
+	}
+	return (0);
+}
