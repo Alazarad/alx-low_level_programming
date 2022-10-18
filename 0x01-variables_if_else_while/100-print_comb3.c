894c432fd458dae0eda3cd1fe222db00101b5a24
Alazarad <Alazarad>
2022-10-17 19:06:41 -0700

    print combination of two digits

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+/**
+*main - Prints all combinations of two two digits with,
+* and space followed by new line
+*
+*Return: returns 0
+*/
+int main(void)
+{
+	int n1, n2;
+
+	for (n1 = 0; n1 <= 98; n1++)
+	{
+		for (n2 = n1 + 1; n2 <= 99; n2++)
+		{
+			putchar((n1 / 10) + '0');
+			putchar((n2 % 10) + '0');
+			putchar(32);
+			putchar((n1 / 10) + '0');
+			putchar((n2 % 10) + '0');
+			if (n1 / 10 != 9 || n1 % 10 != 8)
+			{
+				putchar(44);
+				putchar(32);
+			}
+		}
+	}
+	putchar(10);
+	return (0);
+}
