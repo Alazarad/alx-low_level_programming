972ad6e93e806dd2630d29529be216622da95101
Alazarad <Alazarad>
2022-10-19 15:26:16 -0700

    determine if a number is lower case

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _islower-entry point
+ * @c: is supposed to be inputed
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+		return (1);
+	else
+		return (0);
+}
