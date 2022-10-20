fd9ea9bffd4f76a9733d517e6afda4ce974bbd85
Alazarad <Alazarad>
2022-10-19 18:14:40 -0700

    tell the time

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,41 @@
+#include "main.h"
+
+/**
+ * jack_bauer-print _putchar
+ *
+ * Return: 0
+ */
+void jack_bauer(void)
+{
+	int h, m;
+
+	for (h = 0; h <= 23; h++)
+	{
+		for (m = 0; m <= 59; m++)
+		{
+			if (h < 10)
+			{
+				_putchar('0');
+				_putchar(h + '0');
+			}
+			else if (h >= 10)
+			{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			}
+			if (m < 10)
+			{
+			_putchar(':');
+			_putchar('0');
+			_putchar(m + '0');
+			}
+			else if (m > 10)
+			{
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			}
+			_putchar('\n');
+		}
+	}
+}
