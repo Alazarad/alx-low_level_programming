917877331e753d1eca69e6ae7b1e4f809ce2b240
Alazarad <Alazarad>
2022-10-22 17:10:04 -0700

    advanced task

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,30 +1,30 @@
 #include <stdio.h>
 /**
-*main - Prints all combinations of two two digits with,
-* and space followed by new line
-*
-*Return: returns 0
-*/
+ * main-entry point
+ *
+ *Return: always 0
+ */
+
 int main(void)
 {
-	int n1, n2;
+	int a, b;
 
-	for (n1 = 0; n1 <= 98; n1++)
+	for (a = 0; a < 10; a++)
 	{
-		for (n2 = n1 + 1; n2 <= 99; n2++)
+		for (b = 0; b < 10; b++)
 		{
-			putchar((n1 / 10) + '0');
-			putchar((n2 % 10) + '0');
-			putchar(32);
-			putchar((n1 / 10) + '0');
-			putchar((n2 % 10) + '0');
-			if (n1 / 10 != 9 || n1 % 10 != 8)
+			if (a != b && a < b)
 			{
-				putchar(44);
-				putchar(32);
+			putchar(a + '0');
+			putchar(b + '0');
+			if (a != 8)
+			{
+				putchar(',');
+				putchar(' ');
+			}
 			}
 		}
 	}
-	putchar(10);
+	putchar('\n');
 	return (0);
 }
