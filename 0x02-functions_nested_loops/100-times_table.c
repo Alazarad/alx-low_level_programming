d71fb8685b938a7a4443aff62b9460fbf6a078dd
Alazarad <Alazarad>
2022-10-25 13:24:28 -0700

    timestable

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -16,38 +16,31 @@ void print_times_table(int n)
 		for (b = 0; b <= n; b++)
 		{
 			c = a * b;
-			if (c < 10)
+			if (b == 0)
+				_putchar('0');
+			else if (c < 10)
 			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(c + '0');
-				if (b != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-					_putchar(' ');
-				}
 			}
 			else if (c < 100 && c >= 10)
 			{
+				_putchar(',');
+				_putchar(' ');
+				_putchar(' ');
 				_putchar(c / 10 + '0');
 				_putchar(c % 10 + '0');
-				if (b != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
-				}
 			}
 			else if (c >= 100)
 			{
+				_putchar(',');
+				_putchar(' ');
 				_putchar(c / 100 + '0');
 				_putchar((c / 10) % 10 + '0');
 				_putchar(c % 10 + '0');
-				if (b != n)
-				{
-					_putchar(',');
-					_putchar(' ');
-				}
 			}
 		}
 		_putchar('\n');
