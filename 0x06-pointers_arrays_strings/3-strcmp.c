ae4cb579c0326ac388b56a44a111a17450f6f183
Alazarad <Alazarad>
2022-11-05 16:08:10 -0700

    correct betty

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -3,7 +3,7 @@
  * _strcmp- compares two strings
  * @s1: string 1
  * @s2: string 2
- * Return:
+ * Return: value of difference
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -15,5 +15,5 @@ int _strcmp(char *s1, char *s2)
 			break;
 	}
 	b = s1[a] - s2[a];
-	return(b);
+	return (b);
 }
