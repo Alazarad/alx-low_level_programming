ffe0b1bb13b10cae306a8880d8bddbebba285a69
Alazarad <Alazarad>
2022-11-05 17:51:44 -0700

    submit 6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -8,13 +8,24 @@
  */
 char *cap_string(char *string)
 {
-	int a;
+	int a, b;
+	int characters[] = {32, 9, 10, 44, 59, 46, 33, 63, 34, 40, 123, 125};
 
+	if (string[0] >= 'a' && string[0] <= 'z')
+		string[0] = string[0] - 32;
 	for (a = 0; string[a] != '\0'; a++)
 	{
-		if (isspace(string[a]) != 0)
+		for (b = 0; b < 13; b++)
+		{
+			if (string[a] == characters[b])
+			{
 			if (string[a + 1] <= 'z' && string[a + 1] >= 'a')
+			{
 				string[a + 1] = string[a + 1] - 32;
+				break;
+			}
+			}
+		}
 
 	}
 	return (string);
