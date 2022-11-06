f396c4329804b72c8c86cf2e29e9b0bb2d0d5143
Alazarad <Alazarad>
2022-11-05 18:58:34 -0700

    submit 101-print_number.c

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * print_number - print an integer, without using long, arrays, or pointers
+ * @n: number to be printed
+ */
+
+void print_number(int n)
+{
+	unsigned int a, b, c = n;
+	double d = 1;
+
+	if (n == 0)
+		_putchar('0');
+	else
+	{
+		if (n < 0)
+		{
+			c = n * -1;
+			_putchar('-');
+		}
+
+		while (d <= c)
+			d *= 10;
+		a = d / 10;
+
+		while (a >= 1)
+		{
+			b = c / a;
+			_putchar(b + '0');
+			c = (c - (a * b));
+			a /= 10;
+		}
+	}
+}
