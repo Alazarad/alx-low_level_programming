c7b3acf347bb6375e3be513cf0b8672457e57c67
Alazarad <Alazarad>
2022-11-05 18:48:00 -0700

    submit 100-rot13.c

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * rot13 - encode string using rot13
+ * @s: string to encode
+ * Return: encoded string
+ */
+
+char *rot13(char *s)
+{
+	char a[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXY";
+	char b[] = "nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM";
+	int i, j;
+
+	for (j = 0; s[j] != '\0'; j++)
+	{
+		i = 0;
+		while (a[i] != '\0' && s[j] != a[i])
+			i++;
+
+		if (s[j] == a[i])
+			s[j] = b[i];
+	}
+
+	return (s);
+}
