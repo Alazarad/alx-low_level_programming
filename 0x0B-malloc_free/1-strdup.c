d346e739bd67e7110125f1f8dd7bfbe86633d398
Alazarad <Alazarad>
2022-11-08 14:19:30 -0800

    submit 1-strdup.c

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strdup- copy a string in a new memory
+ * @str: string
+ * Return: pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *dest;
+	unsigned int count, a;
+
+	if (str == 0)
+	{
+		return (NULL);
+	}
+
+	for (count = 0; str[count] != '\0'; count++)
+		count = count + 1;
+
+	dest = (char *) malloc(count * sizeof(char));
+
+	if (dest == 0)
+	{
+		return (NULL);
+	}
+	for (a = 0; str[a] != '\0'; a++)
+		dest[a] = str[a];
+	return (dest);
+}
