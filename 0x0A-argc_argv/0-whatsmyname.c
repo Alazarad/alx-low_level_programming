28b2403012ec5e925c12d0d7a8dc49c638dbecdc
Alazarad <Alazarad>
2022-11-16 15:09:21 -0800

    try fixing unknown

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
 /**
- * main- print the file name during runtime
+ * main - print the file name during runtime
  * @argc: argc
- * @argv:argv
- * Return: nothing
+ * @argv: argv
+ * Return: 0
  */
-int main(int argc , char *argv[])
+int main(int argc __attribute__((unused)), char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
