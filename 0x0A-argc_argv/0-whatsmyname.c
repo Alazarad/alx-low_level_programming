0f5bd357a369aeec61c78e49ced7d891bf969b3f
Alazarad <Alazarad>
2022-11-16 14:55:22 -0800

    submit 0-whatsmyname.c

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- print the file name during runtime
+ * @argc: argc
+ * @argv:argv
+ * Return: nothing
+ */
+int main(int argc , char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
