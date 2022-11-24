58c7f772445fedc9de46c2b300e71b10162c8413
Alazarad <Alazarad>
2022-11-24 15:18:17 -0800

    submit 4-strpbrk.c

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -11,13 +11,13 @@ char *_strpbrk(char *s, char *accept)
 {
 	int count1, count2;
 
-	for (count1 = 0;*(s + count1) != '\0';count1++)
+	for (count1 = 0; *(s + count1) != '\0'; count1++)
 	{
-		for (count2 = 0;*(accept + count2) != '\0';count2++)
+		for (count2 = 0; *(accept + count2) != '\0'; count2++)
 		{
 			if (s[count1] == accept[count2])
 				return (s + count1);
 		}
 	}
 	return (0);
-}		
+}
