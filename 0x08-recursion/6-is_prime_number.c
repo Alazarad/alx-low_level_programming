b9ccfc540402577fd7fb7c1b8994842bf5266b05
Alazarad <Alazarad>
2022-12-01 16:14:45 -0800

    betty correction

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -11,25 +11,21 @@ int is_prime_number(int n)
 {
 	if (n <= 1)
 		return (0);
-	else
-		return (_prime(n, 2));
+	return (_prime(n, 2));
 }
 /**
- * _prime- help function for is_prime_number
+ * _prime - help function for is_prime_number
  * @n : input
  * @a : possible divisors
- * Retrun: 1 if n is prime
+ * Return: 1 if n is prime
  * 0 if not
  */
 int _prime(int n, int a)
 {
 	if (n / a < 2)
 		return (1);
+	if (n % a == 0)
+		return (0);
 	else
-	{
-		if (n % a == 0)
-			return (0);
-		else
-			return (_prime(n, a + 1));
-	}
+		return (_prime(n, a + 1));
 }
