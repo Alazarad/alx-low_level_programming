bff2228a56b0251fa7d44dc3f822631ac38df313
Alazarad <demissiealazarad@gmail.com>
2023-04-19 04:49:04 -0700

    task 2 of function pointer

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,16 @@
+#include "function_pointers.h"
+
+/**
+ * void array_iterator - pass elements of an array to a function
+ * @array : array to be passed
+ * @size : size of array
+ * @action : funtion to implement
+ * Return: void
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	size_t a = 0;
+
+	for (a = 0; a < size; a++)
+		action(array[a]);
+}
