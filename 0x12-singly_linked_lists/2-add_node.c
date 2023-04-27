16e4b8674b0ba609357fbff67061fcca6a218b60
Alazarad <demissiealazarad@gmail.com>
2023-04-27 06:07:56 -0700

    correction of task 2 of singly linked list

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -10,15 +10,19 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
-	int a = 0;
 
 	if (new == NULL)
 		return (NULL);
 	new->str = strdup(str);
+	if (new->str == NULL)
+	{
+		free(new);
+		return (NULL);
+	}
 
-	while (str[a] != '\0')
-		a++;
-	new->len = a;
+	new->len = 0;
+	while (new->str[new->len] != '\0')
+		new->len++;
 	new->next = *head;
 
 	*head = new;
