e3364edf5ca77c5604828aec1c461e9b8b2f3ecc
Alazarad <demissiealazarad@gmail.com>
2023-04-27 06:37:11 -0700

    correction of task 4 of singly linked list

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -8,11 +8,10 @@
 void free_list(list_t *head)
 {
 	list_t *del = head;
-	list_t *tmp = malloc(sizeof(list_t));
 
 	while (del)
 	{
-		tmp = del;
+		list_t *tmp = del;
 		del = del->next;
 		free(tmp);
 	}
