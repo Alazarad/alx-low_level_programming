e5750445d7bb5f6c90445c78e93c86117849ecef
Alazarad <demissiealazarad@gmail.com>
2023-04-27 04:47:27 -0700

    correction of task 0 of singly linked list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -16,7 +16,7 @@ size_t print_list(const list_t *h)
 			printf("[%u] [%s]\n", h->len, h->str);
 		}
 		else
-			printf("[%d] (%s)\n", 0, "nil");
+			printf("[0] (nil)\n");
 		length++;
 		h = h->next;
 	}
