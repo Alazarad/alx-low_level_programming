d5c0bffe70fbc104a59ab8387f1e559d7f33e7b1
Alazarad <demissiealazarad@gmail.com>
2023-04-27 04:39:36 -0700

    task 0 of singly linked list

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * print_list - print all elements of a list
+ * @h: head of list
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t length = 0;
+
+	while (h != NULL)
+	{
+		if (h->str != NULL)
+		{
+			printf("[%u] [%s]\n", h->len, h->str);
+		}
+		else
+			printf("[0] (nil)\n");
+		length++;
+		h = h->next;
+	}
+
+	return (length);
+}
