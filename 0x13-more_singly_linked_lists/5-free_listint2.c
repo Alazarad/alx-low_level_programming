6926ceb2c153422e72be5741f67827b4862bfe9b
Alazarad <demissiealazarad@gmail.com>
2023-05-02 00:05:57 -0700

    task 5 of more singly linked list

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - free a list while setting head to NULL
+ * @head: first element of the list
+ * Return: void
+ */
+
+void free_listint2(listint_t **head)
+{
+	listint_t *tmp = *head;
+
+	while (*head != NULL)
+	{
+		tmp = *head;
+		(*head) = (*head)->next;
+		free(tmp);
+	}
+}
