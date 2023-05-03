4cf1628d8d0f2b5c39df4aa2b097bf8b211a5065
Alazarad <demissiealazarad@gmail.com>
2023-05-03 01:26:10 -0700

    correction of task 5 of more singly linked list

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -8,10 +8,11 @@
 
 void free_listint2(listint_t **head)
 {
-	listint_t *tmp = *head;
+	listint_t *tmp;
 
-	if (*head == NULL)
+	if (head == NULL || *head == NULL)
 		return;
+	tmp = *head;
 	while (*head != NULL)
 	{
 		tmp = *head;
